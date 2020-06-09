f4bf62560e42e922da583d999de9f07cfbc9f0c1
mbpmm <marianomcr92@gmail.com>
2020-06-09 00:31:40 -0300

    Added range weapon functionality (WIP)

---
+++ EmptyScreamProject/Assets/Scripts/RangeWeapon.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeWeapon : MonoBehaviour
+{
+    public float damage;
+    public float range;
+    public float fireRate;
+    public float impactForce;
+
+    public Camera cam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impact;
+
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1")&& Time.time>= nextFire)
+        {
+            nextFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.transform.position,cam.transform.forward,out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (hit.rigidbody!=null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            if (target!=null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            GameObject impactGO = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
---
+++ EmptyScreamProject/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health<=0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+
+    }
+}
