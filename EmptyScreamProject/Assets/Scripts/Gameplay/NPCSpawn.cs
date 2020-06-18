44595e4ca89dd5003affd42629cdfcd99b58c977
frankvega2010 <franco_vega@msn.com>
2020-06-18 13:17:38 -0300

    Added enemy spawner

---
+++ EmptyScreamProject/Assets/Scripts/Gameplay/NPCSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawn : MonoBehaviour
+{
+    public KeyCode spawnKey;
+    public GameObject npcToSpawn;
+    public Transform spawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(spawnKey))
+        {
+            Instantiate(npcToSpawn, spawnPosition.position, spawnPosition.rotation);
+        }
+    }
+}
